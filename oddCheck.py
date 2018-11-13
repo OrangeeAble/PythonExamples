74c4b98dde11f494988bcccda4842b4aafe2deb6
OrangeeAble <omar_mohamed900@yahoo.com>
2018-11-13 18:39:46 +0300

    Updated OddChecker
    Now actually checks for odd numbers

--- oddCheck.py
+++ oddCheck.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 import random
 '''
-  _       _           _   _  ___        
- (_)     (_)         | | (_)/ _ \       
-  _ _ __  _  ___  ___| |_ _| | | |_ __  
- | | '_ \| |/ _ \/ __| __| | | | | '_ \ 
+  _       _           _   _  ___
+ (_)     (_)         | | (_)/ _ \
+  _ _ __  _  ___  ___| |_ _| | | |_ __
+ | | '_ \| |/ _ \/ __| __| | | | | '_ \
  | | | | | |  __/ (__| |_| | |_| | | | |
  |_|_| |_| |\___|\___|\__|_|\___/|_| |_|
-        _/ |                            
-       |__/                             
+        _/ |
+       |__/
        '''
        # Odd Check from ArrayList
 def OddChecker(number):
-		if (number % 2 ==0):
-			print "odd! : ", number
+		if (number % 2 != 0):
+			print("odd! : " + str(number))
 
-	 
+
 
 myArray = []
 for x in range(1,101):
 	myArray.append(x)
 
 
-print myArray
+print(myArray)
 for numbers in myArray:
-	OddChecker(numbers)	
-
-	
+	OddChecker(numbers)
 
 
 
